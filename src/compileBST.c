e174785843da5e61d958a7ed8bd90537a878bb25
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2016-11-07 11:37:18 +0100

    m

--- src/compileBST.c
+++ src/compileBST.c
@@ -26,80 +26,80 @@
  * \returns { 0 if succeeds and prints C code implementing an optimal ABR on stdout; exit code otherwise}
  */
 int main (int argc, char *argv[]) {
-  long n = 0 ; // Number of elements in the dictionary  FILE *freqFile = NULL ; // File that contains n positive integers defining the relative frequence of dictinary elements 
-  
-  if(argc != 3){
-    fprintf(stderr, "!!!!! Usage: ./compileBST n  originalFile !!!!!\n");
-    exit(EXIT_FAILURE); /* indicate failure.*/
-  }
+    long n = 0 ; // Number of elements in the dictionary  FILE *freqFile = NULL ; // File that contains n positive integers defining the relative frequence of dictinary elements 
 
-  { // Conversion of parameter n in a long 
-    int codeRetour = EXIT_SUCCESS;
-    char *posError;
-    long resuLong;
-    n = atol(argv[1] ) ;
-   
-    assert(argc >= 2);
-    // Conversion of argv[1] en long
-    resuLong = strtol(argv[1], &posError, 10);
-    // Traitement des erreurs
-    switch (errno)
-    {
-      case EXIT_SUCCESS :
-         // Conversion du long en int
-         if (resuLong > 0)
-         {
-            n = (long)resuLong;
-            fprintf(stderr, "Number of elements in the dictionary: %ld\n", n);         
-         }
-         else
-         {
-            (void)fprintf(stderr, "%s cannot be converted into a positive integer matching the number of elements in the dicionary.\n", argv[1]) ; 
-            codeRetour = EXIT_FAILURE;
-         }
-      break;
-      
-      case EINVAL :
-         perror(__func__);
-         (void)fprintf(stderr, "%s does not match a long integer value. \n", argv[1]);
-         codeRetour = EXIT_FAILURE;
-      break;
-      
-      case ERANGE :
-         perror(__func__);
-         (void)fprintf(stderr, "%s does not fit in a long int :\n" "out of bound [%ld;%ld]\n",
-                       argv[1], LONG_MIN, LONG_MAX);
-         codeRetour = EXIT_FAILURE;
-      break;
-      default :
-         perror(__func__);
-         codeRetour = EXIT_FAILURE;
-    } // switch (errno)
-    if  (codeRetour != EXIT_SUCCESS) return codeRetour ;
-  }
+    if(argc != 3){
+        fprintf(stderr, "!!!!! Usage: ./compileBST n  originalFile !!!!!\n");
+        exit(EXIT_FAILURE); /* indicate failure.*/
+    }
 
-  FILE* freqFile = fopen(argv[2] , "r" ); // freqFile = fopen(argv[2] , "r" );
-  if (freqFile==NULL) {fprintf (stderr, "!!!!! Error opening originalFile !!!!!\n"); exit(EXIT_FAILURE);}
+    { // Conversion of parameter n in a long 
+        int codeRetour = EXIT_SUCCESS;
+        char *posError;
+        long resuLong;
+        n = atol(argv[1] ) ;
 
-  // TO BE COMPLETED
+        assert(argc >= 2);
+        // Conversion of argv[1] en long
+        resuLong = strtol(argv[1], &posError, 10);
+        // Traitement des erreurs
+        switch (errno)
+        {
+            case EXIT_SUCCESS :
+                // Conversion du long en int
+                if (resuLong > 0)
+                {
+                    n = (long)resuLong;
+                    fprintf(stderr, "Number of elements in the dictionary: %ld\n", n);         
+                }
+                else
+                {
+                    (void)fprintf(stderr, "%s cannot be converted into a positive integer matching the number of elements in the dicionary.\n", argv[1]) ; 
+                    codeRetour = EXIT_FAILURE;
+                }
+                break;
 
-  /* Debut Modification */
-  //double* probabilites = parser_fichier((int) n, freqFile);
-  int abr[n][2];
-  for (int i = 0; i < n; i ++) {
-      abr[i][0] = -1;
-      abr[i][1] = -1;
-  }
-  int racines[n][n];
-  //f(0, ((int) n) - 1, probabilites, &racines);
-  //abr_opt(0, (int) n - 1, racines, &abr);
-  affiche_abr((int) n, abr);
-  
-  /* Fin Modification */
-  
-  fclose(freqFile);
+            case EINVAL :
+                perror(__func__);
+                (void)fprintf(stderr, "%s does not match a long integer value. \n", argv[1]);
+                codeRetour = EXIT_FAILURE;
+                break;
+
+            case ERANGE :
+                perror(__func__);
+                (void)fprintf(stderr, "%s does not fit in a long int :\n" "out of bound [%ld;%ld]\n",
+                        argv[1], LONG_MIN, LONG_MAX);
+                codeRetour = EXIT_FAILURE;
+                break;
+            default :
+                perror(__func__);
+                codeRetour = EXIT_FAILURE;
+        } // switch (errno)
+        if  (codeRetour != EXIT_SUCCESS) return codeRetour ;
+    }
+
+    FILE* freqFile = fopen(argv[2] , "r" ); // freqFile = fopen(argv[2] , "r" );
+    if (freqFile==NULL) {fprintf (stderr, "!!!!! Error opening originalFile !!!!!\n"); exit(EXIT_FAILURE);}
+
+    // TO BE COMPLETED
+
+    /* Debut Modification */
+    //double* probabilites = parser_fichier((int) n, freqFile);
+    int abr[n][2];
+    for (int i = 0; i < n; i ++) {
+        abr[i][0] = -1;
+        abr[i][1] = -1;
+    }
+    int racines[n][n];
+    //f(0, ((int) n) - 1, probabilites, &racines);
+    //abr_opt(0, (int) n - 1, racines, &abr);
+    affiche_abr((int) n, abr);
+
+    /* Fin Modification */
+
+    fclose(freqFile);
 
 
-  return 0;
+    return 0;
 }
 
--- src/fonctions.c
+++ src/fonctions.c
@@ -3,21 +3,21 @@
 
 bool estChiffre(char c) {
     switch (c) {
-    case '0' ... '9':
-	return true;
-    default :
-	return false;	
+        case '0' ... '9':
+            return true;
+        default :
+            return false;	
     }    
 }
 
 /* Fonction qui parse le fichier pour renvoyé le tableau des probabilités associées aux noeuds 
-   Remarque : On suppose que le fichier est au format adapté i.e ("o_1, o_2, ... , o_n")
-   Pas de controle de format. */
+Remarque : On suppose que le fichier est au format adapté i.e ("o_1, o_2, ... , o_n")
+Pas de controle de format. */
 double* parser_fichier(int n, FILE* f) {
     /*
-      - requiert : f non NULL
-    */
-    
+       - requiert : f non NULL
+       */
+
     /* Tableau des probabilites */
     double *p = malloc(n * sizeof(double));
     char c;
@@ -26,58 +26,58 @@ double* parser_fichier(int n, FILE* f) {
     int somme = 0;
     bool lectureNombre = false;
     do
-        {
+    {
+        c = fgetc(f);
+        while (estChiffre(c)) {
+            if (!lectureNombre) {
+                nombre = atoi(&c);
+                lectureNombre = true;
+            } else {
+                nombre = nombre*10 + atoi(&c);
+            }
             c = fgetc(f);
-	    while (estChiffre(c)) {
-		if (!lectureNombre) {
-		    nombre = atoi(&c);
-		    lectureNombre = true;
-		} else {
-		    nombre = nombre*10 + atoi(&c);
-		}
-		c = fgetc(f);
-	    }
-	    if (lectureNombre) {
-		lectureNombre = false;
-		printf("Nombre : %d\n", nombre);
-		p[indice] = (double) nombre;
-		somme += nombre;
-		indice++;
-	    }
-        } while (c != EOF); 
+        }
+        if (lectureNombre) {
+            lectureNombre = false;
+            printf("Nombre : %d\n", nombre);
+            p[indice] = (double) nombre;
+            somme += nombre;
+            indice++;
+        }
+    } while (c != EOF); 
     printf("Somme : %d\n Tableau: ", somme);
- 
+
     for (int i=0; i<n; i++) {
-	printf(" %f ", p[i]);
-	p[i] = p[i] / (double) somme;
+        printf(" %f ", p[i]);
+        p[i] = p[i] / (double) somme;
     }
     for (int i=0; i<n; i++) {
-	printf(" %f ", p[i]);
+        printf(" %f ", p[i]);
     }
     return p;
 }
-   
+
 double f(int i, int j, double probabilites[], int ***racines) {
     if (j < i) {
-	return 0;
+        return 0;
     } else if (i == j) {
-	return probabilites[i];
+        return probabilites[i];
     } else {
-	int min = -1;
-	int s = 0;
-	int indice;
-	for (int n = i; n <= j; n++) {
-	    s += probabilites[n];
-	}
-	for (int k = i; k <= j; k++) {
-	    int res = f(i, k-1, probabilites, racines) + f(k+1, j, probabilites, racines);
-	    if (min == -1 || res + s < min) {
-		min = res;
-		indice = k;
-	    }
-	}
-	(*racines)[i][j] = indice;
-	return min;
+        int min = -1;
+        int s = 0;
+        int indice;
+        for (int n = i; n <= j; n++) {
+            s += probabilites[n];
+        }
+        for (int k = i; k <= j; k++) {
+            int res = f(i, k-1, probabilites, racines) + f(k+1, j, probabilites, racines);
+            if (min == -1 || res + s < min) {
+                min = res;
+                indice = k;
+            }
+        }
+        (*racines)[i][j] = indice;
+        return min;
     }
 }
 
@@ -87,23 +87,23 @@ void abr_opt(int i, int j, int **racines, int ***abr) {
     (*abr) [r][1] = -1;
 
     if (i < r-1) {
-	(*abr) [r][0] = racines[i][r-1];
-	abr_opt(i, r-1, racines, abr);
+        (*abr) [r][0] = racines[i][r-1];
+        abr_opt(i, r-1, racines, abr);
     }
 
     if (r < j) {
-	(*abr) [r][1] = racines[r][j];
-	abr_opt(r, j, racines, abr);
-	}
+        (*abr) [r][1] = racines[r][j];
+        abr_opt(r, j, racines, abr);
+    }
 }
 
 void affiche_abr(int n, int *[2]abr) {
     printf("\n{");
     for (int i = 0; i < n; i ++) {
-	printf(" {%d, %d}", abr[i][0], abr[i][1]);
-	if (i != n-1) {
-	    printf(",");
-	}
+        printf(" {%d, %d}", abr[i][0], abr[i][1]);
+        if (i != n-1) {
+            printf(",");
+        }
     }
     printf(" }\n");
 }
