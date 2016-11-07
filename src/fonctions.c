056e7fbcf1ee543407d073f62eec5051c53d93d8
Mickaël Ranaivoarisoa <mickael.ranaivoarisoa@ensimag.grenoble-inp.fr>
2016-11-07 22:29:32 +0100

    m

--- src/fonctions.c
+++ src/fonctions.c
@@ -76,40 +76,47 @@ double f(int i, int j, int n, double probabilites[], int **racines) {
             s += probabilites[a];
         }
         for (int k = i; k <= j; k++) {
-            int res = f(i, k-1, n, probabilites, racines) + f(k+1, j, n, probabilites, racines);
-            if (min == -1 || res + s < min) {
-                min = res + s;
+            int res = f(i, k-1, n, probabilites, racines) + f(k+1, j, n, probabilites, racines) + s;
+            if (min == -1 || res <= min) {
+		min = res;
                 indice = k;
             }
         }
         *((*racines) + i*n + j) = indice;
+    
         return min;
     }
 }
 
-void abr_opt(int i, int j, int n, int *racines, int ***abr) {
+void abr_opt(int i, int j, int n, int *racines, int (*abr)[2]) {
     int r = racines[i*n + j];
+    if (r==-1) return;
     //(*abr) [r] = -1;
     //(*abr) [r + 1] = -1;
-
+    //printf("\n %d %d %d %d\n", r, n, i, j);
     if (i < r-1) {
-        (*abr)[r][0] = racines[i*n + r-1]; // Fils gauche
-        abr_opt(i, r-1, n, racines, abr);
+        //(*abr)[r][0] = racines[i*n + r-1]; // Fils gauche
+        (abr)[r][0] = racines[i*n + r-1];
+	abr_opt(i, r-1, n, racines, abr);
     }
 
     if (r+1 < j) {
-        (*abr)[r][1] = racines[r*n + j]; // Fils droite
-        abr_opt(r+1, j, n, racines, abr);
+        //(*abr)[r][1] = racines[r*n + j]; // Fils droite
+	//	printf("\n %d %d %d\n", r, n, j);
+        (abr)[r][1] = racines[(r+1)*n + j];
+	abr_opt(r+1, j, n, racines, abr);
     }
 }
 
-void affiche_abr(int n, int (*abr)[2]) {
-    printf("\n{");
+void affiche_abr(int n, int (*abr)[2], int *racines) {
+
+    printf("static int BSTroot = %d;\n", racines[n-1]);
+    printf("static int BSTtree[%d][2] = {\n", n);
     for (int i = 0; i < n; i++) {
         printf(" {%d, %d}", abr[i][0], abr[i][1]);
         if (i != n-1) {
-            printf(",");
+            printf(",\n");
         }
     }
-    printf(" }\n");
+    printf(" };\n");
 }
--- src/fonctions.h
+++ src/fonctions.h
@@ -12,8 +12,8 @@ double* parser_fichier(int n, FILE* f);
 
 double f(int i, int j, int n, double probabilites[], int **racines);
 
-void abr_opt(int i, int j, int n, int *racines, int ***abr);
+void abr_opt(int i, int j, int n, int *racines, int (*abr)[2]);
 
-void affiche_abr(int n, int (*abr)[2]);
+void affiche_abr(int n, int (*abr)[2], int *racines);
 
 #endif
