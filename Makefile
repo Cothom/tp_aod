fa2f13b7d46a7ba7c7280a0e05146476d8d6c5e2
Thomas Conte <Thomas.Conte@ensimag.grenoble-inp.fr>
2016-11-07 13:15:40 +0100

    +benchmarks

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=cc
 LATEXC=pdflatex
 DOCC=doxygen
-CFLAGS=-g -Wall 
+CFLAGS=-g -Wall -std=c99
 
 REFDIR=.
 SRCDIR=$(REFDIR)/src
@@ -19,7 +19,7 @@ all: binary #report doc
 
 # $(BINDIR)/compileBST: $(CSOURCE)
 $(BINDIR)/compileBST: $(CSOURCE) $(CCOMPLEMENT)
-	$(CC) $(CFLAGS)  $^ -o $@
+	$(CC) $(CFLAGS) $^ -o $@
 
 %.pdf: $(LATEXSOURCE)
 	$(LATEXC) -output-directory $(REPORTDIR) $^ 
--- src/fonctions.c
+++ src/fonctions.c
@@ -35,16 +35,6 @@ double* parser_fichier(int n, FILE* f) {
             } else {
                 nombre = nombre*10 + atoi(&c);
             }
-            c = fgetc(f);
-            while (estChiffre(c)) {
-                if (!lectureNombre) {
-                    nombre = atoi(&c);
-                    lectureNombre = true;
-                } else {
-                    nombre = nombre*10 + atoi(&c);
-                }
-                c = fgetc(f);
-            }
             if (lectureNombre) {
                 lectureNombre = false;
                 //printf("Nombre : %d\n", nombre);
@@ -52,67 +42,68 @@ double* parser_fichier(int n, FILE* f) {
                 somme += nombre;
                 indice++;
             }
-        } while (c != EOF); 
-        //printf("Somme : %d\n Tableau: ", somme);
+        }
+    } while (c != EOF); 
+    //printf("Somme : %d\n Tableau: ", somme);
 
-        for (int i=0; i<n; i++) {
-            //printf(" %f ", p[i]);
-            p[i] = p[i] / (double) somme;
-        }
-        for (int i=0; i<n; i++) {
-            //printf(" %f ", p[i]);
-        }
-        return p;
+    for (int i=0; i<n; i++) {
+        //printf(" %f ", p[i]);
+        p[i] = p[i] / (double) somme;
     }
-
-    double f(int i, int j, int n, double probabilites[], int **racines) {
-        if (j < i) {
-            return 0;
-        } else if (i == j) {
-            (*racines)[i*n + j] = i;
-            return probabilites[i];
-        } else {
-            int min = -1;
-            int s = 0;
-            int indice;
-            for (int a = i; a <= j; a++) {
-                s += probabilites[a];
-            }
-            for (int k = i; k <= j; k++) {
-                int res = f(i, k-1, n, probabilites, racines) + f(k+1, j, n, probabilites, racines);
-                if (min == -1 || res + s < min) {
-                    min = res + s;
-                    indice = k;
-                }
-            }
-            (*racines)[i*n + j] = indice;
-            return min;
-        }
+    for (int i=0; i<n; i++) {
+        //printf(" %f ", p[i]);
     }
+    return p;
+}
 
-    void abr_opt(int i, int j, int n, int *racines, int **abr) {
-        int r = racines[i*n + j];
-        //(*abr) [r] = -1;
-        //(*abr) [r + 1] = -1;
-
-        if (i < r-1) {
-            (*abr) [r] = racines[i*n + r-1]; // Fils gauche
-            abr_opt(i, r-1, n, racines, abr);
+double f(int i, int j, int n, double probabilites[], int **racines) {
+    if (j < i) {
+        return 0;
+    } else if (i == j) {
+        (*racines)[i*n + j] = i;
+        return probabilites[i];
+    } else {
+        int min = -1;
+        int s = 0;
+        int indice;
+        for (int a = i; a <= j; a++) {
+            s += probabilites[a];
         }
-
-        if (r+1 < j) {
-            (*abr) [r + 1] = racines[r*n + j]; // Fils droite
-            abr_opt(r+1, j, n, racines, abr);
-        }
-    }
-
-    void affiche_abr(int n, int *abr) {
-        printf("\n{");
-        for (int i = 0; i < n; i++) {
-            printf(" {%d, %d}", abr[i], abr[i+1]);
-            if (i != n-1) {
-                printf(",");
+        for (int k = i; k <= j; k++) {
+            int res = f(i, k-1, n, probabilites, racines) + f(k+1, j, n, probabilites, racines);
+            if (min == -1 || res + s < min) {
+                min = res + s;
+                indice = k;
             }
         }
-        printf(" }\n");
+        (*racines)[i*n + j] = indice;
+        return min;
     }
+}
+
+void abr_opt(int i, int j, int n, int *racines, int **abr) {
+    int r = racines[i*n + j];
+    //(*abr) [r] = -1;
+    //(*abr) [r + 1] = -1;
+
+    if (i < r-1) {
+        (*abr) [r] = racines[i*n + r-1]; // Fils gauche
+        abr_opt(i, r-1, n, racines, abr);
+    }
+
+    if (r+1 < j) {
+        (*abr) [r + 1] = racines[r*n + j]; // Fils droite
+        abr_opt(r+1, j, n, racines, abr);
+    }
+}
+
+void affiche_abr(int n, int *abr) {
+    printf("\n{");
+    for (int i = 0; i < n; i++) {
+        printf(" {%d, %d}", abr[i], abr[i+1]);
+        if (i != n-1) {
+            printf(",");
+        }
+    }
+    printf(" }\n");
+}
